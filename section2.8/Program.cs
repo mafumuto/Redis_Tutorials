06c6b8f06eebe08cd430e3a21a4dde007c1275d1
Mahmut Akyalçın <88111783+mafumuto@users.noreply.github.com>
2024-08-14 10:53:36 +0300

    practiced Redis Transaction Condition

--- section2.8/Program.cs
+++ section2.8/Program.cs
@@ -16,8 +16,22 @@ transaction.SortedSetAddAsync("person:name:Steve", "person:1", 0);
 transaction.SortedSetAddAsync("person:postal_code:32999", "person:1", 0);
 transaction.SortedSetAddAsync("person:age", "person:1", 32);
 
-var success = transaction.Execute();
-Console.WriteLine($"Transaction Successful: {success}");
+var success1 = transaction.Execute();
+Console.WriteLine($"Transaction Successful - 1: {success1}");
+
+var person1Entry = db.HashGetAll("person:1");
+transaction.AddCondition(Condition.HashEqual("person:1", "age", 32));
+transaction.HashIncrementAsync("person:1", "age");
+transaction.SortedSetIncrementAsync("person:age", "person:1", 1);
+
+
+var success2 = transaction.Execute();
+Console.WriteLine($"Transaction Successful - 2: {success2}");
+
+var ageSet = db.SortedSetScan("person:age");
+
+Console.WriteLine($"Person:1 field: \n+ {string.Join("\n+ ", person1Entry)}\n");
+Console.WriteLine($"Age Set field: \n- {string.Join("\n- ", ageSet)}\n");
 
 
 // end coding challenge
