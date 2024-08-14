c16f88b4bb188a05ce3295074cba2d5bf39c5070
Mahmut Akyalçın <88111783+mafumuto@users.noreply.github.com>
2024-08-14 09:22:26 +0300

    practice Redis Lua Prepared Script

--- section2.7/Program.cs
+++ section2.7/Program.cs
@@ -5,5 +5,25 @@ var db = muxer.GetDatabase();
 
 // TODO for Coding Challenge Start here on starting-point branch
 
+#region Prepared Script
+
+
+var scriptText = @"
+    local id = redis.call('incr', @id_key)
+    local key = 'key:' .. id
+    redis.call('set', key, @value)
+    return key
+";
+
+LuaScript script = LuaScript.Prepare(scriptText);
+
+RedisResult key1 = db.ScriptEvaluate(script, new { id_key = (RedisKey)"autoIncrement", value = "Trial value" });
+RedisResult key2 = db.ScriptEvaluate(script, new { id_key = (RedisKey)"autoIncrement", value = "Another Trial value" });
+
+
+Console.WriteLine($"Key 1: {key1}");
+Console.WriteLine($"Key 2: {key2}");
+#endregion
+
 
 //end coding challenge
